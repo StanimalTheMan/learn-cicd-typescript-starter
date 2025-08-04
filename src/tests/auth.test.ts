2188db83df6d2794ea3b8143173eee0f1981d648
Stan Choi <sjc591@nyu.edu>
2025-08-04 11:52:10 -0400

    write some unit tests

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,22 @@
+import { getAPIKey } from "src/api/auth";
+import { describe, expect, test } from "vitest";
+
+describe("getAPIKey", () => {
+  test("empty authHeader results in null", () => {
+    expect(getAPIKey({})).toBeNull();
+  });
+
+  test("authorization header of less than 2 in length results in null / is invalid", () => {
+    expect(getAPIKey({ authorization: "Bearer" })).toBeNull();
+  });
+
+  test("authorization header must start with ApiKey", () => {
+    expect(getAPIKey({ authorization: "Bearer sdfskldghslg" })).toBeNull();
+  });
+
+  test("valid authorization header returns key", () => {
+    const apiKey = "skfssdfsdf";
+    expect(getAPIKey({ authorization: `ApiKey ${apiKey}` })).not.toBeNull();
+    expect(getAPIKey({ authorization: `ApiKey ${apiKey}` })).toBe(apiKey);
+  });
+});
